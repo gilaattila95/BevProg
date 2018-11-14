1d4ced046d31d26d9c1eba0211ba423592b0f9ef
gilaattila95 <43230841+gilaattila95@users.noreply.github.com>
2018-11-14 20:44:21 +0100

    Update and rename d to d.c

---
+++ d.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main (void)
+{
+  int i, egy_e;
+  unsigned char b;
+
+  while (read (0, (void *) &b, sizeof (unsigned char)))
+    {
+      if (b == 0x0a)
+        continue;
+      for (i = 0; i < 8; ++i)
+        {
+          egy_e = b & 0x80;
+          if ((egy_e >> 7) == 1)
+            printf ("1");
+          else
+            printf ("0");
+          b <<= 1;
+        }
+    }
+}
