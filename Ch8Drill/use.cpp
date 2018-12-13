f03b3219d48eaecbf595f5fdf42b03cd815d130a
Gila Attila <43230841+gilaattila95@users.noreply.github.com>
2018-12-13 18:55:20 +0100

    Update use.cpp

--- Ch8Drill/use.cpp
+++ Ch8Drill/use.cpp
@@ -1,7 +1,18 @@
 #include "my.h"
-
-int main () {
+int main()
+{
     foo = 7;
     print_foo();
-    print(99);
+   
+    int x = 7;
+    int y = 9;
+    swap_v(x, y);
+    
+   
+    int x2 = 7;
+    int y2 = 9;
+    swap_r(x2, y2);
+ 
+    
+    return 0;
 }
